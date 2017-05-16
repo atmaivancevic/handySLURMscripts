eabc41d52f1cea740c87199fbf2948f2dc3ed6da
Atma Maria Ivancevic <a1211880@l03.phoenix.adelaide.edu.au>
2017-05-16 16:48:31 +0930

    output scatterplots for both cpu time and elapsed

--- plotJobStats.R
+++ plotJobStats.R
@@ -1,19 +1,20 @@
 #!/usr/bin/env Rscript
 
-# R script to make a simple scatter plot of job stats
+# R script to make a scatter plot of mem used vs elapsed (real) time
 # Example usage:
-# Rscript --vanilla plotJobStats.R BWA_highmem_jobs_nr.txt BWA_highmem_jobs.pdf
+# Rscript --vanilla plotJobStats.R BWA_highmem_jobs_nr.txt BWA_highmem_jobs_MEMvsElapsedTime.pdf BWA_highmem_jobs_MEMvsCPUTime.pdf
 
 args = commandArgs(trailingOnly=TRUE)
-# args[1] = input file name
-# args[2] = output file name
+# args[1] = input file
+# args[2] = output file 1
+# args[3] = output file 2
 
 # test if there is at least one argument: if not, return an error
 if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 } else if (length(args)==1) {
   # default output file
-  args[2] = "all"
+  args[2] = "output.pdf"
 }
 
 setwd("/fast/users/a1211880/jobStats/")
@@ -23,20 +24,11 @@ title <- gsub(pattern = "_nr.txt",replacement = "", x = args[1])
 pdata <- plot(x = data$ElapsedTimeHr, y = data$MemUsedGB, main = title, xlab="Elapsed Time (hr)", ylab="Memory Used (gb)", xlim = range(pretty(c(0,max(data$ElapsedTimeHr)))), ylim = range(pretty(c(0,max(data$MemUsedGB)))))
 dev.off()
 
+setwd("/fast/users/a1211880/jobStats/")
+pdf(file = args[3])
+data <- read.table(args[1], sep ="\t", header = T)
+title <- gsub(pattern = "_nr.txt",replacement = "", x = args[1])
+pdata <- plot(x = data$CPUTimeHr, y = data$MemUsedGB, main = title, xlab="CPU Time (hr)", ylab="Memory Used (gb)", xlim = range(pretty(c(0,max(data$CPUTimeHr)))), ylim = range(pretty(c(0,max(data$MemUsedGB)))))
+dev.off()
+
 q(save="yes")
-
-
-
-### Alternative way of doing it
-
-#setwd("/fast/users/a1211880/")
-#files <- list.files(path = "jobStats/")
-
-#pdf(file = "slurmPlots.pdf", onefile = TRUE)
-#for(i in 1:length(files)){
-#  print(i)
-#  data <- read.table(paste("jobStats/", files[i], sep ="\t", header = T))
-#  name <- gsub(pattern = "_nr.txt",replacement = "", x = files[i])
-#  pdata <- plot(x = data$ElapsedTimeHr, y = data$MemUsedGB, main = name)
-#}
-#dev.off()
